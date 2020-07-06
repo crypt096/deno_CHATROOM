0933ae719eec2c2ebadf19dca1e472bd610ebbf2
crypt096 <acvjetan@gmail.com>
2020-07-07 00:11:03 +0200

    Initial code commit.

---
+++ ws/chatroom.ts
@@ -0,0 +1,42 @@
+import {
+  WebSocket,
+  isWebSocketCloseEvent,
+} from 'https://deno.land/std/ws/mod.ts';
+import { v4 } from 'https://deno.land/std/uuid/mod.ts';
+
+let sockets = new Map<string, WebSocket>();
+
+interface BroadcastObj {
+  name: string;
+  mssg: string;
+}
+
+// broadcast events to all clients
+const broadcastEvent = (obj: BroadcastObj) => {
+  sockets.forEach((ws: WebSocket) => {
+    ws.send(JSON.stringify(obj));
+  });
+};
+
+const chatConnection = async (ws: WebSocket) => {
+  // add new ws connection to map
+  const uid = v4.generate();
+  sockets.set(uid, ws);
+
+  for await (const ev of ws) {
+    console.log(ev);
+
+    // delete socket if conn is closed
+    if (isWebSocketCloseEvent(ev)) {
+      sockets.delete(uid);
+    }
+
+    // create ev object if ev is string
+    if (typeof ev === 'string') {
+      let evObj = JSON.parse(ev);
+      broadcastEvent(evObj);
+    }
+  }
+};
+
+export { chatConnection };
